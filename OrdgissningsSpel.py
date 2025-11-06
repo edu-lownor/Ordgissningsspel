28db78d0d353430e35dd42ba00e925451a5caadd
Lowe Nordstrand <lowe.nordstrand@skola.taby.se>
2025-11-06 16:08:52 +0000

    Lade till grundläggande funktionalitet

--- OrdgissningsSpel.py
+++ OrdgissningsSpel.py
@@ -1,7 +1,17 @@
 import random
 
 ordlista = ["Mask", "Kanin", "Julafton", "pepparkakor", "Stenfisk"]
-
 hemligt_ord = random.choice(ordlista)
 
+gissning = ["_"] * len(hemligt_ord)
+gissade_bokstäver = set()
+
 print("Välkommen till Ordgissningsspelet!\nDu ska få gissa ordet, det har", len(hemligt_ord), "bokstäver.")
+
+antal = 10
+while "_" in gissning:
+    print("\nNuvarande ord:", " ".join(gissning))
+    bokstav = input("Gissa en bokstav: ").lower()
+    antal -= 1
+    print(f"du har {antal} gissningar kvar")
+
