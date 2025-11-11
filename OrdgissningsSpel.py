1631a8c4ee557c9044f0c866fc5895aecf257c6f
edu-lineri <linus.eriksson.07@gmail.com>
2025-11-11 09:35:45 +0100

    Nu fungerar själva spelet plus att man inte kan skriva samma bokstav två gånger
    Nästa gång bör vi fixa så man inte kan skriva mer än ett tecken.

--- OrdgissningsSpel.py
+++ OrdgissningsSpel.py
@@ -14,4 +14,38 @@ while "_" in gissning:
     bokstav = input("Gissa en bokstav: ").lower()
     antal -= 1
     print(f"du har {antal} gissningar kvar")
+#Kommer inte riktigt ihåg hur man commita så jag tar det nästa gång, här är koden från idag.
 
+
+
+
+
+import random
+
+ordlista = ["Mask", "Kanin", "Julafton", "Pepparkakor", "Stenfisk"]
+hemligt_ord = random.choice(ordlista).lower()
+
+gissning = ["_"] * len(hemligt_ord)
+gissade_bokstäver = set()
+
+print("Välkommen till Ordgissningsspelet!")
+print(f"Du ska gissa ett ord som har {len(hemligt_ord)} bokstäver.\n")
+
+antal = 10
+while "_" in gissning and antal>=0:
+    print("Nuvarande ord:", " ".join(gissning))
+    print(f"Du har {antal} gissningar kvar.")
+    bokstav = input("Gissa en bokstav: ").lower()
+    if bokstav in gissade_bokstäver:
+        print("Du har redan använt denna bokstav, försök igen")
+        antal+=1
+    gissade_bokstäver.add(bokstav)
+    antal-=1
+    for i in range(len(hemligt_ord)):
+        if hemligt_ord[i] == bokstav:
+            gissning[i] = bokstav
+
+if "_" not in gissning:
+    print(f"Grattis du gissade rätt ord\nDet korrekta ordet var {hemligt_ord}")
+if antal<=1:
+    print(f"Du har tyvär använt upp alla dina gissningar och därmed avslutat spelet, det korrekta ordet var {hemligt_ord}")
