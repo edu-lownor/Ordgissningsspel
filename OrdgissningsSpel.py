64d8876598b4bfd6447aa2f7384c4edf084d725d
edu-lineri <linus.eriksson.07@gmail.com>
2025-11-13 15:16:50 +0100

    Tredje session
    typ klart

--- OrdgissningsSpel.py
+++ OrdgissningsSpel.py
@@ -49,3 +49,60 @@ if "_" not in gissning:
     print(f"Grattis du gissade rätt ord\nDet korrekta ordet var {hemligt_ord}")
 if antal<=1:
     print(f"Du har tyvär använt upp alla dina gissningar och därmed avslutat spelet, det korrekta ordet var {hemligt_ord}")
+
+
+#Tredje tillfälle
+
+import random
+j = 1
+while j == 1:
+    ordlista = ["Mask", "Kanin", "Julafton", "Pepparkakor", "Stenfisk"]
+    hemligt_ord = random.choice(ordlista).lower()
+
+    gissning = ["_"] * len(hemligt_ord)
+    gissade_bokstäver = set()
+    felgissningar=[]
+    print("Välkommen till Ordgissningsspelet!")
+    print(f"Du ska gissa ett ord som har {len(hemligt_ord)} bokstäver.\n")
+
+    antal = 10
+    while "_" in gissning and antal>=0:
+        print("Nuvarande ord:", " ".join(gissning))
+        print(f"Du har {antal} gissningar kvar.")
+        antal-=1
+        bokstav = input("Gissa en bokstav: ").lower()
+        if len(bokstav)>1:
+             print("Du ska bara använda en bokstav")
+             antal+=1
+        if bokstav in gissade_bokstäver:
+            print("Du har redan använt denna bokstav, försök igen")
+            antal+=1
+        if bokstav in hemligt_ord:
+            print("Denna bokstav fanns i ordet")
+        else:
+            print("Denna bokstav fanns inte i ordet")
+            if bokstav in felgissningar:
+                continue
+            else:
+                felgissningar.append(bokstav)
+                fel_ihop2=" ".join(felgissningar)
+            print(f"Dina felgissningar är {fel_ihop2}")
+        gissade_bokstäver.add(bokstav)
+
+        for i in range(len(hemligt_ord)):
+            if hemligt_ord[i] == bokstav:
+                gissning[i] = bokstav
+        continue
+
+
+    if "_" not in gissning:
+        print(f"Grattis du gissade rätt ord\nDet korrekta ordet var {hemligt_ord}")
+    if antal<=1:
+        print(f"Du har tyvär använt upp alla dina gissningar och därmed avslutat spelet, det korrekta ordet var {hemligt_ord}")
+
+    Restart=input("Vill du starta om spelet, J/N").lower()
+    if j:
+        continue
+    else:
+        j += 1
+print("Tack föratt du körde! :D")
